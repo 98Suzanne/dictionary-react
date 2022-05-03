fa5d9a164955a8fd16ab13dd470ebd253c446033
98Suzanne <s.dinten@upcmail.nl>
2022-05-03 09:41:00 +0200

    Testing dictionary API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,18 +1,28 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword}`);
+
+    let apiUrl = `ttps://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
 
-  return (
+  //api.dictionaryapi.dev/api/v2/entries/en/<word>
+
+  https: return (
     <div className="Dictionary">
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
