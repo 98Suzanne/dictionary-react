0631b49ea3927df1695f455ace03a7e1d2094102
98Suzanne <s.dinten@upcmail.nl>
2022-05-06 12:18:16 +0200

    Added synonyms + basic styling

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
@@ -20,14 +21,33 @@ export default function Dictionary() {
     setKeyword(event.target.value);
   }
 
-  //api.dictionaryapi.dev/api/v2/entries/en/<word>
-
   return (
-    <div className="Dictionary">
-      <form onSubmit={search}>
-        <input type="search" onChange={handleKeywordChange} />
-      </form>
-      <Results results={results} />
+    <div className="SearchContent">
+      <section>
+        <h1>What word do you want to look up?</h1>
+        <form onSubmit={search}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Search word..."
+                onChange={handleKeywordChange}
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn SearchButton"
+              />
+            </div>
+          </div>
+        </form>
+        <div className="WordSuggestions">
+          Suggested words: forest, cat, advertisement...
+        </div>
+        <Results results={results} />
+      </section>
     </div>
   );
 }
